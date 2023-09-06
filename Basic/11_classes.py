da4c0d849ff1754eee5b09a03ade795d53db883b
sebastian-riggio <sebastian.riggio@gmail.com>
2023-09-06 08:21:50 +0200

    first commit

---
+++ Basic/11_classes.py
@@ -0,0 +1,40 @@
+
+### Classes ###
+
+# Definición
+
+class MyEmptyPerson:
+    pass  # Para poder dejar la clase vacía
+
+
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+# Clase con constructor, funciones y popiedades privadas y públicas
+
+
+class Person:
+    def __init__(self, name, surname, alias="Sin alias"):
+        self.full_name = f"{name} {surname} ({alias})"  # Propiedad pública
+        self.__name = name  # Propiedad privada
+
+    def get_name(self):
+        return self.__name
+
+    def walk(self):
+        print(f"{self.full_name} está caminando")
+
+
+my_person = Person("Sebas", "Riggio")
+print(my_person.full_name)
+print(my_person.get_name())
+my_person.walk()
+
+my_other_person = Person("Sebas", "Riggio", "Sebasriggio")
+print(my_other_person.full_name)
+my_other_person.walk()
+my_other_person.full_name = "Pedro Lopez (El loco de los perros)"
+print(my_other_person.full_name)
+
+my_other_person.full_name = 555
+print(my_other_person.full_name)
